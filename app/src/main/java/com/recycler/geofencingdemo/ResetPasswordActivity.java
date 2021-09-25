4e471761289e9d484ac80e93b380c6c1cda2cf27
Tarun_Satheesh <tarunthetitan03@gmail.com>
2021-09-25 07:01:54 +0530

    4th commit

--- app/src/main/java/com/recycler/geofencingdemo/ResetPasswordActivity.java
+++ app/src/main/java/com/recycler/geofencingdemo/ResetPasswordActivity.java
@@ -1,7 +1,6 @@
 package com.recycler.geofencingdemo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +9,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
